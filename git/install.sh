d46de2ef89d857c68ea9f77fe56a266219fd9ead
Cyril Wanner <info@cyr.li>
2021-05-11 16:47:36 +0200

    Update git config

--- git/install.sh
+++ git/install.sh
@@ -3,6 +3,9 @@
 # enable git commit signing globally
 git config --global commit.gpgsign true
 
+# set pull strategy
+git config --global pull.rebase false
+
 # set committer name and email
 if [ ! $(git config --global user.email) ]; then
     git config --global user.email "info@cyr.li"
